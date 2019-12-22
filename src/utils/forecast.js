ae4331db716f7eecbe503d07cc98a3b3b846d15d
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2019-12-22 14:08:20 -0800

    added more data to the app

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,15 +3,20 @@ const request = require("request");
 const forecast = (long, lat, callback) => {
   const url = `https://api.darksky.net/forecast/a3dd7647a87af518202324c32cf5f93b/${lat},${long}`;
   request({ url: url, json: true }, (error, response) => {
+    console.log(response.body);
     if (error) {
       callback("theres been a problem", undefined);
     } else if (response.body.error) {
       callback("not results found", undefined);
     } else {
-      const { temperature, precipProbability } = response.body.currently;
+      const {
+        temperature,
+        precipProbability,
+        summary
+      } = response.body.currently;
       callback(
         undefined,
-        `it is currently ${precipProbability} degrees out. there is a ${temperature}% chance of rain`
+        `it is currently ${summary} and ${temperature} degrees out. there is a ${precipProbability}% chance of rain`
       );
     }
   });
